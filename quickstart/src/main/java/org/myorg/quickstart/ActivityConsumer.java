011e47f39a261df6dbc34d80e0ff2d81eaae3738
Alban Tiacoh <alban.kdf@gmail.com>
2021-06-29 13:30:30 +0200

    Add process window with aggregate baseline

--- quickstart/src/main/java/org/myorg/quickstart/ActivityConsumer.java
+++ quickstart/src/main/java/org/myorg/quickstart/ActivityConsumer.java
@@ -2,11 +2,15 @@ package org.myorg.quickstart;
 
 import java.sql.Timestamp;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -48,38 +52,70 @@ public class ActivityConsumer {
         FlinkKafkaConsumer<Activity> kafkaSource = new FlinkKafkaConsumer<>(topics, new ActivityDeserializationSchema(), properties);
         kafkaSource.assignTimestampsAndWatermarks(
                 WatermarkStrategy
-                .forBoundedOutOfOrderness(Duration.ofSeconds(10)));
+                .forBoundedOutOfOrderness(Duration.ofSeconds(1)));
         DataStream<Activity> stream = env.addSource(kafkaSource);
 
-        DataStream<Tuple2<String, Integer>> uidAnalysis = stream
-                .keyBy(Activity::getUid)
-                .window(TumblingEventTimeWindows.of(Time.seconds(1)))
-                .process(new ProcessWindowFunction<Activity, Tuple2<String, Integer>, String, TimeWindow>() {
+        DataStream<Tuple3<String, String, String>> uidAnalysis = stream
+                .map(new MapFunction<Activity, Tuple3<String, String, String>>() {
                     @Override
-                    public void process(String key, Context context, Iterable<Activity> iterable, Collector<Tuple2<String, Integer>> collector) throws Exception {
-                        Integer count = 0;
-                        Activity previous = null;
-                        System.out.println("key of key by : "+key+" and key from iterable : "+key);
-                        for (Activity in: iterable) {
-                            if(in.isClick()){
-                                count++;
-                            }
-                            if (previous != null) {
-                                System.out.println(" previous "+previous.timestamp+" in "+in.timestamp+" time diff in seconds "+computeReactionTime(previous.timestamp, in.timestamp));
-                            }
-                            if (previous != null && previous.isDisplay() && in.isClick() && computeReactionTime(previous.timestamp, in.timestamp) < MIN_REACTION_TIME) {
-                                System.out.println("Alert for user : "+key+" with a reaction time too fast (<1 seconds)");
-                            }
-                            previous = in;
-                        }
-                        if (count > MAX_CLICKS_PER_WINDOW)
-                        {
-                            System.out.println("Alert for user : "+previous.uid+" with a number of clicks per window too high");
-                        }
-                        System.out.println("Window: "+context.window().toString()+" watermark: "+context.currentWatermark());
-                        collector.collect(new Tuple2<String, Integer>(context.window().toString(), count));
+                    public Tuple3<String, String, String> map(Activity activity) throws Exception {
+                        return new Tuple3<>(activity.getUid(), activity.getTimestamp(), activity.getEventType());
                     }
-                });
+                })
+                .keyBy(0)
+                .window(TumblingEventTimeWindows.of(Time.minutes(10)))
+                .aggregate(new AggregateFunction<Tuple3<String, String, String>, Object, Tuple3<String, String, String>>() {
+                               @Override
+                               public Object createAccumulator() {
+                                   return new Integer(0);
+                               }
+
+                               @Override
+                               public Object add(Tuple3<String, String, String> stringStringStringTuple3, Object o) {
+                                   return null;
+                               }
+
+                               @Override
+                               public Tuple3<String, String, String> getResult(Object o) {
+                                   return null;
+                               }
+
+                               @Override
+                               public Object merge(Object o, Object acc1) {
+                                   return null;
+                               }
+                           }
+                        , new ProcessWindowFunction<Tuple3<String, String, String>, Tuple3<String, String, String>, Tuple, TimeWindow>() {
+                            @Override
+                            public void process(Tuple key, Context context, Iterable<Tuple3<String, String, String>> iterable, Collector<Tuple3<String, String, String>> collector) throws Exception {
+                                Integer count = 0;
+                                Tuple3<String, String, String> previous = null;
+                                System.out.println(" Start of Window: " + context.window().toString() + " watermark: " + context.currentWatermark() + " with key : " + key + "and key in iteratror " + iterable.iterator().next().f0);
+                                //System.out.println("key of key by "+key);
+                                //System.out.println("let's see the event for this key in the window");
+                                for (Tuple3<String, String, String> in : iterable) {
+                                    System.out.println("key of iterable " + in.f0);
+                                    if (in.f2.equals("click")) {
+                                        count++;
+                                    }
+                                    if (previous != null) {
+                                        System.out.println("diff in compute time is " + computeReactionTime(previous.f1, in.f1) + "for time1 : " + previous.f1 + " with uid : " + previous.f0 + " and time2 : " + in.f1 + " with uid : " + in.f0);
+                                    }
+                                    if (previous != null && previous.f2.equals("display") && in.f2.equals("click") && computeReactionTime(previous.f1, in.f1) < MIN_REACTION_TIME) {
+                                        System.out.println("Alert for user : " + "key.getField(0)" + " with a reaction time too fast (<1 seconds)");
+                                    }
+                                    previous = in;
+                                }
+                                if (count > MAX_CLICKS_PER_WINDOW) {
+                                    System.out.println("Alert for user : " + "key.getField(0)" + " with a number of clicks per window too high");
+                                }
+                                //System.out.println("End of Window: "+context.window().toString()+" watermark: "+context.currentWatermark());
+                                collector.collect(new Tuple3<String, String, String>("", "", ""));
+                            }
+                        });
+
+
+        uidAnalysis.print();
 
         /*uidAnalysis
                 .addSink(new AlertSink())
@@ -98,11 +134,10 @@ public class ActivityConsumer {
      * @return time difference in seconds
      */
     public static double computeReactionTime(String timestamp1, String timestamp2) {
-        Timestamp timestamp_1 = new Timestamp(Long.parseLong(timestamp1));
-        Timestamp timestamp_2 = new Timestamp(Long.parseLong(timestamp2));
+        Long timestamp_1 = Long.parseLong(timestamp1);
+        Long timestamp_2 = Long.parseLong(timestamp2);
         // time difference in seconds
-        long milliseconds = Math.abs(timestamp_2.getTime() - timestamp_1.getTime());
-        double seconds = (double) milliseconds / 10000000;
-        return seconds;
+        long secondsDifference = Math.abs(timestamp_2 - timestamp_1);
+        return secondsDifference;
     }
 }
--- quickstart/src/main/java/org/myorg/tests/ActivityConsumerTest.java
+++ quickstart/src/main/java/org/myorg/tests/ActivityConsumerTest.java
@@ -12,9 +12,9 @@ class ActivityConsumerTest {
     @Test
     @DisplayName("Calculate timestamp difference")
     void timestampDifference(){
-        assertAll(() -> assertEquals(0, ActivityConsumer.computeReactionTime("1624399636000","1624399636000")),
-                () -> assertEquals(0.003, ActivityConsumer.computeReactionTime("1624399636000","1624399666000")),
-                () -> assertEquals(3, ActivityConsumer.computeReactionTime("1624399636000","1624429636000")));
+        assertAll(() -> assertEquals(0, ActivityConsumer.computeReactionTime("1624959723","1624959723")),
+                () -> assertEquals(1, ActivityConsumer.computeReactionTime("1624959723","1624959724")),
+                () -> assertEquals(3, ActivityConsumer.computeReactionTime("1624959723","1624959720")));
     }
 
 }
