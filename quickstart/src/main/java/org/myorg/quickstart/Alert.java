1dd0576707f8a026c299a1e9225c2da0d2f6a4e4
Alban Tiacoh <alban.kdf@gmail.com>
2021-07-01 17:06:51 +0200

    Add Ip Aggregate tests

--- quickstart/src/main/java/org/myorg/quickstart/Alert.java
+++ quickstart/src/main/java/org/myorg/quickstart/Alert.java
@@ -36,4 +36,19 @@ public final class Alert extends Activity {
                 " alertPattern=" + alertPattern +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        // self check
+        if (this == o)
+            return true;
+        // null check
+        if (o == null)
+            return false;
+        // type check and cast
+        if (getClass() != o.getClass())
+            return false;
+        Alert alert = (Alert) o;
+        return this.getAlertPattern().equals(alert.getAlertPattern());
+    }
 }
